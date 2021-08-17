1ad706efa9066f8549bdd34ebd9e5fb47a33a5c3
Ejim Michael <ejimmichael1@gmail.com>
2021-08-17 04:37:34 +0100

    Add sign out functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Imessage from './Imessage';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser, login, logout } from './features/userSlice';
 import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser); 
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+    auth.onAuthStateChanged(authUser =>{
+      if(authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName,
+        })
+        );
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
