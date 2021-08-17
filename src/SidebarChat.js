fe4ae366384bfcfda67c65110a0c2e51149c19a1
Ejim Michael <ejimmichael1@gmail.com>
2021-08-17 16:14:01 +0100

    Add timestamp to chat

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SidebarChat.css';
 import { Avatar } from "@material-ui/core";
+import { useDispatch } from 'react-redux';
+import { setChat } from './features/chatSlice';
 
 function Sidebarchat({id, chatName}) {
+    const dispatch = useDispatch();
+    // const [chatInfo, setChatInfo] = useState([]);
+
     return (
-        <div className="sidebarChat">
+        <div onClick={() => 
+            dispatch(
+                setChat({
+                    chatId: id,
+                    chatName: chatName,
+                })
+            )
+        } className="sidebarChat">
             <Avatar />
             <div className="sidebarChat__info">
                 <h3>{chatName}</h3>
